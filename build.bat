edcc1876378bf3ff6202917410564daf74eb83d1
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2020-09-29 21:40:23 -0700

    remove build.bat and revert latest pull request
    all you need to do to build this is use make lol

--- build.bat
+++
@@ -1,9 +0,0 @@
-set PATH=C:\devkitPro\msys\bin;C:\devkitPro\devkitARM\bin;%PATH%
-set DEVKITARM=/c/devkitPro/devkitARM
-set DEVKITPRO=/c/devkitPro
-set LIBGBA=/c/devkitPro/libgba
-
-make  
-rem >error.txt 2>&1
-pause
-build.bat
--- build.sh
+++
@@ -1,6 +0,0 @@
-export  PATH=/opt/devkitpro/tools/bin:/opt/devkitpro/devkitarm/bin:$PATH
-export DEVKITARM=/opt/devkitpro/devkitARM
-export DEVKITPRO=/opt/devkitpro
-export LIBGBA=/opt/devkitpro/libgba
-
-make
