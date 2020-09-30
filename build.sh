cc75963b256e93f38c3a2de6fbc9e0fda020b41c
izder456 <izder456@protonmail.com>
2020-09-29 23:14:09 -0500

    add bash compile script

---
+++ build.sh
@@ -0,0 +1,6 @@
+export  PATH=/opt/devkitpro/tools/bin:/opt/devkitpro/devkitarm/bin:$PATH
+export DEVKITARM=/opt/devkitpro/devkitARM
+export DEVKITPRO=/opt/devkitpro
+export LIBGBA=/opt/devkitpro/libgba
+
+make
